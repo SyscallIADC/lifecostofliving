d27527022907c4f870b55b8c733e22a1eeee144a
Daniela Martel <niamarttel@gmail.com>
2026-03-31 16:57:49 +0100

    Creamos la tabla para ExchangeRate y creamos el insert para poder introducir los datos

--- src/main/java/database/DatabaseHelper.java
+++ src/main/java/database/DatabaseHelper.java
@@ -1,4 +1,6 @@
 package database;
+import models.ExchangeRate;
+
 import java.sql.*;
 
 public class DatabaseHelper{
@@ -25,7 +27,7 @@ public class DatabaseHelper{
         return instance;
     }
 
-    public Connection getConnection() {ç
+    public Connection getConnection() {
         try {
             if (connection == null | connection.isClosed()) {
                 connection = DriverManager.getConnection(DATABASE_PATH);
@@ -37,13 +39,18 @@ public class DatabaseHelper{
     }
 
     private void createTables() {
-        String sql = "CREATE TABLE [IF NOT EXISTS] currencies (" +
-                "" +
-                "";
+        String sql = "CREATE TABLE [IF NOT EXISTS] exchange-rate (" +
+                "from_currency TEXT NOT NULL, " +
+                "to_currency TEXT NOT NULL, " +
+                "exchange_rate REAL, " +
+                "last_refreshed TEXT, " +
+                "time_zone TEXT," +
+                "PRIMARY KEY (from_currency, to_currency, last_refreshed)" +
+                ");";
         try (Statement statement = connection.createStatement()) {
             statement.execute(sql);
         } catch (SQLException e) {
-            System.out.println("Failed to create table currencies: " + e.getMessage());
+            System.out.println("Failed to create table exchage-rate: " + e.getMessage());
         }
 
         sql = "CREATE TABLE [IF NOT EXISTS] cost-of-living (" +
@@ -55,4 +62,25 @@ public class DatabaseHelper{
             System.out.println("Failed to create table cost-of-living: " + e.getMessage());
         }
     }
+
+    public void insertExchangeRate(ExchangeRate rate){
+        String sql = "INSERT INTO exchange-rate " +
+                "(from_currency, to_currency, exchange_rate, last_refreshed, time_zone)" +
+                "VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, rate.getFromCurrency());
+            pstmt.setString(2, rate.getToCurrency());
+            pstmt.setDouble(3, rate.getExchangeRate());
+            pstmt.setString(4, rate.getLastRefreshed());
+            pstmt.setString(5, rate.getTimeZone());
+        pstmt.executeUpdate();
+
+        }catch (SQLException e){
+            System.out.println("Failed to insert exchange rate: " + e.getMessage());
+
+        }
+    }
 }
+
+
+
